f7f7e55a33626147e1f31b910544af8b446173ba
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2024-07-25 05:54:53 -0700

    scroll buttons added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,15 +2,18 @@
 /* eslint-disable react/prop-types */
 import { IoSearch } from "react-icons/io5";
 import useSelectHook from "../hooks/useSelect";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { IoIosArrowDown } from "react-icons/io";
+import { FaChevronCircleRight } from "react-icons/fa";
+import { FaChevronCircleLeft } from "react-icons/fa";
 
 const Body = () => {
     const [fetchedData, setFetchedData] = useState([]);
     const [type, setType] = useSelectHook("all");
     const [dateRange, setDateRange] = useSelectHook("all");
     const [searchQuery, setSearchQuery] = useState("");
+    const scrollContainerRef = useRef(null);
 
     const fetchRetreats = async () => {
         try {
@@ -19,6 +22,7 @@ const Body = () => {
 
             const filteredData = data.filter(retreat => {
                 const date = new Date(retreat.date);
+                //getting year
                 const year = date.getFullYear();
 
                 let dateMatch = false;
@@ -43,7 +47,7 @@ const Body = () => {
                     retreat.price.toString().toLowerCase().includes(searchQuery.toLowerCase()) ||
                     retreat.location.toLowerCase().includes(searchQuery.toLowerCase());
 
-                console.log(searchMatch);
+                // console.log(searchMatch);
 
                 return dateMatch && typeMatch && searchMatch;
             });
@@ -54,6 +58,19 @@ const Body = () => {
         }
     };
 
+    const scrollLeft = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollBy({ left: -300, behavior: 'smooth' });
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollBy({ left: 300, behavior: 'smooth' });
+        }
+    };
+
+
     useEffect(() => {
         fetchRetreats();
     }, [type, dateRange, searchQuery]);
@@ -62,12 +79,21 @@ const Body = () => {
         <div className="w-full p-6 flex flex-col gap-6">
             <FilterOptions setType={setType} setDateRange={setDateRange} setSearchQuery={setSearchQuery} />
 
-            <div className="flex flex-row item-start gap-4 w-full overflow-auto">
+            <div ref={scrollContainerRef} className="flex flex-row items-stretch gap-4 w-full overflow-auto min-h-[445px] scrollbar-hide relative">
                 {
                     fetchedData.map((data, index) => (
                         <Card data={data} key={index} />
                     ))
                 }
+            </div>
+
+            <div className="flex flex-row justify-center w-full gap-x-4">
+                <button onClick={scrollLeft} className="text-4xl text-blue-700 bg-white rounded-full left-0">
+                    <FaChevronCircleLeft />
+                </button>
+                <button onClick={scrollRight} className="text-4xl text-blue-700 bg-white rounded-full right-0" >
+                    <FaChevronCircleRight />
+                </button>
             </div>
         </div>
     );
@@ -83,7 +109,7 @@ function Card({ data }) {
     };
 
     return (
-        <div className="bg-blue-300 p-4 rounded-xl flex-col flex gap-y-3 min-w-72 max-w-80">
+        <div className="bg-blue-300 p-4 rounded-xl flex-col flex gap-y-3 min-w-72 max-w-72">
             <img src={data.image} alt="image" className="h-52 object-cover rounded-lg" />
 
             <div className="flex flex-row items-center justify-between font-bold text-xl">
@@ -109,7 +135,7 @@ function FilterOptions({ setType, setDateRange, setSearchQuery }) {
         <div className="w-full flex flex-row items-center justify-between">
             <section className="flex flex-row gap-x-5">
                 <div className="flex flex-row items-center relative">
-                    <select {...setDateRange} name="date" id="date" className="px-4 py-2 cursor-pointer pr-10 appearance-none bg-blue-500 text-white rounded-lg">
+                    <select {...setDateRange} name="date" id="date" className="px-4 py-2 cursor-pointer pr-10 appearance-none bg-blue-700 text-white rounded-lg">
                         <option className="bg-white text-black" value="all">Filter by date</option>
                         <option className="bg-white text-black" value="1800-1900">1800-1900</option>
                         <option className="bg-white text-black" value="1900-2000">1900-2000</option>
@@ -118,8 +144,9 @@ function FilterOptions({ setType, setDateRange, setSearchQuery }) {
                     </select>
                     <IoIosArrowDown className="absolute text-xl right-4 text-white" />
                 </div>
+
                 <div className="flex flex-row items-center relative">
-                    <select {...setType} name="type" id="type" className="px-4 py-2 cursor-pointer pr-10 appearance-none bg-blue-500 text-white rounded-lg">
+                    <select {...setType} name="type" id="type" className="px-4 py-2 cursor-pointer pr-10 appearance-none bg-blue-700 text-white rounded-lg">
                         <option className="bg-white text-black" value="all">Filter by type</option>
                         <option className="bg-white text-black" value="yoga">Yoga</option>
                         <option className="bg-white text-black" value="meditation">Meditation</option>
